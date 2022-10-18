cd09791eb9334fbc51fd8ebdbabafdcfdeb1909e
profdambledore <dangeorgemarkdore@gmail.com>
2022-10-18 21:59:23 +0100

    Added Project Pages

--- js/projectView.js
+++ js/projectView.js
@@ -14,8 +14,10 @@ projectButtonOne.addEventListener('mousemove', e => {
 // Add hover functions for project one
 projectButtonOne.onmouseenter = function showHoveredContent() {
     projectContent.style.visibility = 'visible';
-    document.getElementById('hoverContentHeader').textContent = 'Project One';
+    document.getElementById('hoverContentHeader').textContent = 'Pub Simulator Prototype';
     document.getElementById('hoverContentImage').style.backgroundImage = "url('img/catTestImage.jpg')";
+    document.getElementById('hoverPointEngine').style.backgroundImage = "url('icon/UnrealIcon.png')"
+    document.getElementById('hoverPointLanguage').style.backgroundImage = "url('icon/CPPIcon.png')"
 }
 
 projectButtonOne.onmouseleave = function hideHoveredContent() {
@@ -32,7 +34,7 @@ projectButtonTwo.addEventListener('mousemove', e => {
 // Add hover functions for project two
 projectButtonTwo.onmouseenter = function showHoveredContent() {
     projectContent.style.visibility = 'visible';
-    document.getElementById('hoverContentHeader').textContent = 'Project Two';
+    document.getElementById('hoverContentHeader').textContent = 'Physics Based Spaceship Experimentation';
     document.getElementById('hoverContentImage').style.backgroundImage = "url('img/catTestImageTwo.jpg')";
     document.getElementById('hoverPointEngine').style.backgroundImage = "url('icon/UnrealIcon.png')"
     document.getElementById('hoverPointLanguage').style.backgroundImage = "url('icon/CPPIcon.png')"
